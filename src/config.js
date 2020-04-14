f7686dc0671a96e5a3c8e29e3bc5ec490bf8ee75
Mohammed Irshad <irshad@ideabits.se>
2020-04-14 09:24:55 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "mynotes-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://wti5zb9ybc.execute-api.us-east-1.amazonaws.com/prod/",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_XK5V9cqoR",
+    APP_CLIENT_ID: "5qg9v25o5kshgg13ptc3qh45kc",
+    IDENTITY_POOL_ID: "us-east-1:c133bfc6-1bd3-4211-99ef-05fc61d0c147",
+  },
+};
